665c0c3ea451e7e4f84074ee84585a002125abae
aurlab <aurlabnoreply@gmail.com>
2022-11-15 12:53:58 +0100

    adjusting for instr rig 1

---
+++ ingest_instrumentrigTrd01.py
@@ -0,0 +1,40 @@
+import os
+from datetime import datetime
+from influxdb import InfluxDBClient
+
+import config
+import util_file
+from ctd import CTD
+# from adcp import ADCP
+# from lisst_200 import Lisst_200
+
+
+def main():
+
+    print("Starting running ingest_instrumentrigTrd01.py at "
+          + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    logger = util_file.init_logger(config.main_logfile, name='ingest_instrumentrigTrd01')
+    logger.info("\n\n------ Starting sync/ingest.")
+
+    logger.info("Fetching the influxdb clients.")
+    admin_user, admin_pwd = util_file.get_user_pwd(os.path.join(config.secrets_dir, 'influx_admin_credentials'))
+    clients = [
+#        InfluxDBClient(config.az_influx_pc, 8086, admin_user, admin_pwd, 'oceanlab'),
+#        InfluxDBClient(config.sintef_influx_pc, 8086, admin_user, admin_pwd, 'test'),
+        InfluxDBClient(config.ntnu_influx_pc, 8086, admin_user, admin_pwd, 'test'),
+
+    ]
+
+    ctd = CTD(influx_clients=clients)
+    ctd.rsync_and_ingest()
+
+    # lisst = Lisst_200(influx_clients=clients)
+    # lisst.rsync_and_ingest()
+
+    # adcp = ADCP()
+    # adcp.rsync_and_ingest()
+
+
+if __name__ == "__main__":
+    main()
